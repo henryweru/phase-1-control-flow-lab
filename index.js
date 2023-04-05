dad84cf454415bb4b1ce95b4849036ff0c6cbbea
henry <henry.waweru@student.moringaschool.com>
2023-04-05 10:09:08 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,41 +1,30 @@
-function scuberGreetingForFeet(feet){
-  // Write your code here!
-  let result ;
-  if (feet <= 400){
-  result = 'This one is on me!';
-}else if (feet > 2000 && feet <=2500){
-  result = 'I will gladly take your thirty bucks.';
-}else if(feet >2500){
-result ='No can do.'
-}
- return result;
-}
-
-
-function ternaryCheckCity(city){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
   let result;
-  if (city === 'NYC'){
-    result ='Ok, sounds good.'
-  }
-  else {
-    result ='No go.'
+  if (feet <= 400) {
+    result = 'This one is on me!';
+  } else if (feet > 2000 && feet <= 2500) {
+    result = 'I will gladly take your thirty bucks.';
+  } else if (feet > 2500) {
+    result = 'No can do.';
   }
   return result;
 }
 
-function switchOnCharmFromTip(tip){
-  // Write your code here!
-let result;
-switch (tip) {
-  case 'generous':
-    result = 'Thank you so much.';
-    break;
+function ternaryCheckCity(city) {
+  return (city === 'NYC') ? 'Ok, sounds good.' : 'No go.';
+}
+
+function switchOnCharmFromTip(tip) {
+  let result;
+  switch (tip) {
+    case 'generous':
+      result = 'Thank you so much.';
+      break;
     case 'not as generous':
       result = 'Thank you.';
       break;
-      default:
-        result ='Bye.';
-}
-return result;
+    default:
+      result = 'Bye.';
+  }
+  return result;
 }
